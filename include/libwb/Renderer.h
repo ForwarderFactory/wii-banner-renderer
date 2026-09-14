f77deedde7470329071040b990b43bb548488d70
Jacob Nilsson <166003882+jacnils@users.noreply.github.com>
2026-09-14 22:21:43 +0200

    Add files via upload

--- include/libwb/Renderer.h
+++ include/libwb/Renderer.h
@@ -3,9 +3,50 @@
 #include <cstdint>
 #include <vector>
 #include <string>
+#include <array>
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #include <windows.h>
 #endif
+
+struct Rect
+{
+    int x;
+    int y;
+    int width;
+    int height;
+};
+
+struct Point {
+    double x;
+    double y;
+};
+
+constexpr Rect GetCrop(const std::array<Point, 4>& pts) {
+    double min_x = pts[0].x;
+    double max_x = pts[0].x;
+    double min_y = pts[0].y;
+    double max_y = pts[0].y;
+
+    for (const auto& p : pts) {
+        min_x = std::min(min_x, p.x);
+        max_x = std::max(max_x, p.x);
+        min_y = std::min(min_y, p.y);
+        max_y = std::max(max_y, p.y);
+    }
+
+    int width = static_cast<int>(max_x - min_x);
+    int height = static_cast<int>(max_y - min_y);
+
+    width &= ~1;
+    height &= ~1;
+
+    return {
+        static_cast<int>(min_x),
+        static_cast<int>(min_y),
+        width,
+        height
+    };
+}
 
 class Renderer
 {
@@ -17,18 +58,19 @@ public:
     void EndFrame();
     bool SavePNG(const std::string& path, int width, int height);
     void ReadPixelsTo(FILE* output);
+    void ReadPixelsTo(FILE *output, Rect crop) const;
 
-    const uint8_t* Pixels() const
+    [[nodiscard]] const uint8_t* Pixels() const
     {
         return m_pixels.data();
     }
 
-    int Width() const
+    [[nodiscard]] int Width() const
     {
         return m_width;
     }
 
-    int Height() const
+    [[nodiscard]] int Height() const
     {
         return m_height;
     }
